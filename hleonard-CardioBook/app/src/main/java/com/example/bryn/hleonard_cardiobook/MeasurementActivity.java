46bceb2ce45418ace3b04541176795bcf7d3566d
BrynL-F <hleonard@ualberta.ca>
2019-02-02 13:48:08 -0700

    Added Measurement Activity

--- hleonard-CardioBook/app/src/main/java/com/example/bryn/hleonard_cardiobook/MeasurementActivity.java
+++ hleonard-CardioBook/app/src/main/java/com/example/bryn/hleonard_cardiobook/MeasurementActivity.java
@@ -1,13 +1,24 @@
 package com.example.bryn.hleonard_cardiobook;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MeasurementActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_measurement);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(myToolbar);
+
+        getSupportActionBar().setTitle("CardioBook Measurement");
     }
+
+
 }
