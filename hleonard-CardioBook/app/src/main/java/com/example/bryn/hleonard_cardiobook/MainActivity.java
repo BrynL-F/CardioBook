bbcb3eb4578eaa955666ff0ca7b7cd49c3511073
BrynL-F <hleonard@ualberta.ca>
2019-02-02 12:23:08 -0700

    Added new activity

--- hleonard-CardioBook/app/src/main/java/com/example/bryn/hleonard_cardiobook/MainActivity.java
+++ hleonard-CardioBook/app/src/main/java/com/example/bryn/hleonard_cardiobook/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bryn.hleonard_cardiobook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,13 +20,24 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        ImageButton addMeasurementButton = (ImageButton) findViewById(R.id.add);
+        addMeasurementButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Intent intentNewMeasurement = new Intent(MainActivity.this, MeasurementActivity.class);
+                startActivity(intentNewMeasurement);
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                        */
             }
+
+            /*
+                    switchButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ViewGoodTimes.class);
+                startActivity(intent);
+             */
         });
     }
 
